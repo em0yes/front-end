0e8cfbda0ad7035768fb8da59ed1c65b8a4a136e
Jiwon42 <wjdwldnjs145@gmail.com>
2024-11-18 02:26:13 +0900

    BeaconScanner Img 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,43 +4,52 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import PlaceButton from "../components/atoms/PlaceButton";
 
+const PageContainer = styled.div`
+    width: 100%; /* 부모 컨테이너는 항상 화면 전체를 차지 */
+    max-width: 1920px; /* 최대 너비 고정 */
+    margin: 0 auto; /* 중앙 정렬 */
+    position: relative; /* 자식 요소 기준점 설정 */
+`;
+
 const SearchPlaceWrapper = styled.div`
-    width:100%;
-    min-height:840px;
-    display:flex;
-    justify-content:space-evenly;
-    align-items:center;
+    width: 100%;
+    min-height: 840px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    position: relative; /* 상대적인 기준점 설정 */
     flex-wrap: wrap; /* 줄바꿈 허용 */
 `;
+
 const BeaconScannerButton = styled.img`
     z-index: 1;
-    position: fixed;
+    position: absolute; /* 부모 컨테이너(PageContainer)를 기준으로 위치 고정 */
     width: 80px;
     height: 120px;
 
-    /* 뷰포트 기준 상대적인 위치 */
-    left: calc(1920px - 100px); /* 화면 오른쪽에서 약간 떨어지도록 조정 */
-    top: 10vh; /* 화면 위쪽에서 10% 내려오게 설정 */
-
-    /* 중앙 정렬 보정 */
-    transform: translateX(-50%);
+    /* 오른쪽 끝에서 약간 떨어지게 (1920px 범위 초과 방지) */
+    right: max(calc((100% - 1920px) / 2 + 60px), 60px); /* 60px을 최소값으로 설정 */
+    top: 16px; /* 고정된 높이 */
 `;
 
-function Home(){
+function Home() {
     return (
         <>
             <Header 
                 title="장소 선택"
                 username="admin"
             />
-            <Link to="/WorkerMapping"><BeaconScannerButton src="/assets/Icon/Main/BeaconScannerIcon.svg"></BeaconScannerButton></Link>
+            <PageContainer>
+                <Link to="/WorkerMapping">
+                    <BeaconScannerButton src="/assets/Icon/Main/BeaconScannerIcon.svg" />
+                </Link>
 
-            <SearchPlaceWrapper>
-                <PlaceButton placeName="7호관" placeImg = "/assets/Icon/Location/Place_7.svg" />
-                <PlaceButton placeName="도서관" placeImg = "/assets/Icon/Location/LibraryLocationPin.svg"/>
-            </SearchPlaceWrapper>
+                <SearchPlaceWrapper>
+                    <PlaceButton placeName="7호관" placeImg="/assets/Icon/Location/Place_7.svg" />
+                    <PlaceButton placeName="도서관" placeImg="/assets/Icon/Location/LibraryLocationPin.svg" />
+                </SearchPlaceWrapper>
+            </PageContainer>
         </>
-
     );
 }
 
